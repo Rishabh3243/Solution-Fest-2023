9c6624244a47d65eb1a01773ba281bd5e2f02d0b
Rishabh Bharatbhai Parmar <rishabhpar7@gmail.com>
2023-02-25 20:01:48 +0530

    Nav Bar

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LUSHIVE</title>
+</head>
+<body>
+    <?php include_once 'Nav/Nav.html' ;?>
+</body>
+</html>
