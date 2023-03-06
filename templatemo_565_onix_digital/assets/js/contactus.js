5fd745de1d40adc261dc7fee81ee8c6a88ebab73
Rishabh Bharatbhai Parmar <rishabhpar7@gmail.com>
2023-03-06 14:19:53 +0530

    the

--- templatemo_565_onix_digital/assets/js/contactus.js
+++ templatemo_565_onix_digital/assets/js/contactus.js
@@ -1,14 +1,10 @@
   function sendMail() {
-    var params = {
+    var templateParams = {
       name: document.getElementById("name").value,
       surname: document.getElementById("surname").value,
       email: document.getElementById("email").value,
-      message: document.getElementById("comment").value,
+      message: document.getElementById("website").value
     };
   
-    emailjs
-      .send("service_bl7gn5d","template_egg1j46",params)
-      .then(function (res) {
-        alert("Message Sent Successfully !");
-      });
+    emailjs.send('service_bl7gn5d','template_egg1j46',templateParams);
   }
